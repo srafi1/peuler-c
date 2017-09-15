9ff0765775bd04d58049e5c8e8fcf56066de9dee
srafi1 <shakilrafi0@gmail.com>
2017-09-15 11:19:47 -0400

    problems 1 and 2

--- peuler.c
+++ peuler.c
@@ -4,9 +4,26 @@ int problem1() {
     int i;
     int sum = 0;
     for (i = 0; i < 1000; i++) {
-        if (i/3 == 0 || i/5 == 0) {
+        //printf("checking for %d \n", i);
+        if (i%3 == 0 || i%5 == 0) {
             sum = sum + i;
-            printf("New sum after adding %d: %d \n", i, sum);
+            //printf("New sum after adding %d: %d \n", i, sum);
+        }
+    }
+    return sum;
+}
+
+int problem2() {
+    int fib1 = 1;
+    int fib2 = 1;
+    int swap = 0;
+    int sum = 0;
+    while (fib2 < 4000000) {
+        swap = fib1;
+        fib1 = fib2;
+        fib2 += swap;
+        if (fib2 % 2 == 0) {
+            sum += fib2;
         }
     }
     return sum;
@@ -14,4 +31,5 @@ int problem1() {
 
 int main() {
     printf("Problem 1: %d \n", problem1());
+    printf("Problem 2: %d \n", problem2());
 }
